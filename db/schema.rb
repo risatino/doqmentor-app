00c93031cc656298416eb0c4895ccfdc881b04a3
risatino <charissatolentino@gmail.com>
2016-10-11 21:24:59 -0500

    add Images model table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012021312) do
+ActiveRecord::Schema.define(version: 20161012022410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20161012021312) do
   create_table "gigs", force: :cascade do |t|
     t.integer  "client_id"
     t.integer  "document_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "contract_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
