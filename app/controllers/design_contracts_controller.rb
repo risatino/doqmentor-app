d9eb831fd837fa52dbcec26e1d294d98fb241247
risatino <charissatolentino@gmail.com>
2016-10-12 23:33:23 -0500

    updates to routes and contract controllers

--- app/controllers/design_contracts_controller.rb
+++ app/controllers/design_contracts_controller.rb
@@ -1,2 +1,57 @@
 class DesignContractsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @design_contracts = DesignContract.all
+  end
+
+  def new
+    @design_contract = DesignContract.new
+  end
+
+  def create
+    @design_contract = DesignContract.create(designer: params[:designer],
+                                             client: params[:client],
+                                             gig_id: params[:gig_id],
+                                             title: params[:title],
+                                             due_date: params[:due_date],
+                                             specs: params[:specs])
+    
+    if @design_contract.save
+      flash[:success] = "Your document has been created"
+      redirect_to "/design-docs/#{@design_contract.id}"
+    else
+      render 'new.html.erb'
+    end
+    
+  end
+
+  def show
+    @design_contract = DesignContract.find(params[:id])
+  end
+
+  def edit
+    @design_contract = DesignContract.find(params[:id])
+  end
+
+  def update
+    @design_contract = DesignContract.find(params[:id])
+    @design_contract.update(designer: params[:designer],
+                            client: params[:client],
+                            gig_id: params[:gig_id],
+                            title: params[:title],
+                            due_date: params[:due_date],
+                            specs: params[:specs])
+
+    flash[:success] = "Your contract has been updated."
+    redirect_to "/design-docs/#{@design_contract.id}"  
+  end
+
+  def destroy
+    @design_contract = DesignContract.find(params[:id])
+    @design_contract.destroy
+    
+    flash[:warning] = "Your contract has been deleted."
+    redirect_to '/design-docs' 
+  end
 end
--- app/controllers/legal_contracts_controller.rb
+++ app/controllers/legal_contracts_controller.rb
@@ -1,2 +1,63 @@
 class LegalContractsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @legal_contracts = LegalContract.all
+  end
+
+  def new
+    @legal_contract = LegalContract.new
+  end
+
+  def create
+    @legal_contract = LegalContract.create(videographer: params[:videographer],
+                                           hiring_attorney: params[:hiring_attorney],
+                                           firm: params[:firm],
+                                           recording_venue: params[:recording_venue],
+                                           due_date: params[:due_date],
+                                           instructions: params[:instructions],
+                                           witness: params[:witness],
+                                           ticket_id: params[:ticket_id],
+                                           media_format: params[:media_format])
+    
+    if @legal_contract.save
+      flash[:success] = "Your document has been created"
+      redirect_to "/legal-docs/#{@legal_contract.id}"
+    else
+      render 'new.html.erb'
+    end
+    
+  end
+
+  def show
+    @legal_contract = LegalContract.find(params[:id])
+  end
+
+  def edit
+    @legal_contract = LegalContract.find(params[:id])
+  end
+
+  def update
+    @legal_contract = LegalContract.find(params[:id])
+    @legal_contract.update(videographer: params[:videographer],
+                           hiring_attorney: params[:hiring_attorney],
+                           firm: params[:firm],
+                           recording_venue: params[:recording_venue],
+                           due_date: params[:due_date],
+                           instructions: params[:instructions],
+                           witness: params[:witness],
+                           ticket_id: params[:ticket_id],
+                           media_format: params[:media_format])
+
+    flash[:success] = "Your contract has been updated."
+    redirect_to "/legal-docs/#{@legal_contract.id}"  
+  end
+
+  def destroy
+    @legal_contract = LegalContract.find(params[:id])
+    @legal_contract.destroy
+    
+    flash[:warning] = "Your contract has been deleted."
+    redirect_to '/legal-docs' 
+  end
 end
