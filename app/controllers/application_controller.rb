f5ef485eedb68ce5aca155b16ca80fe31dfb175b
risatino <charissatolentino@gmail.com>
2016-10-19 19:17:32 -0500

    fixed errors in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :calculate_cart_count
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -17,11 +16,4 @@ private
     redirect_to '/login' unless current_user
   end
 
-  def calculate_cart_count
-    if current_user
-      @cart_count = current_user.currently_carted.count
-    else
-      @cart_count = 0
-    end
-  end
 end
