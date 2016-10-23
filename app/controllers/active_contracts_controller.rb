aa2db1ca0d5f3a0e1cbf8a0b6006458b68e4f1d6
risatino <charissatolentino@gmail.com>
2016-10-22 23:04:15 -0500

    working on active_contracts and billable_hours

--- app/controllers/active_contracts_controller.rb
+++ app/controllers/active_contracts_controller.rb
@@ -1,2 +1,29 @@
 class ActiveContractsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    if current_user && current_user.currently_carted.any?
+      @active_contracts = current_user.currently_carted
+    else
+      flash[:warning] = "Please add contract to queue."
+      redirect_to '/'
+    end
+  end
+
+  def create
+    ActiveContract.create(product_id: params[:product_id],
+                         quantity: params[:quantity],
+                         user_id: current_user.id,
+                         status: "activated")
+
+    flash[:success] = "Contract has been added to the queue."
+    redirect_to '/active_contracts'
+  end
+
+  def destroy
+    active_contract = ActiveContract.find(params[:id])
+    active_contract.update(status: "removed")
+    flash[:success] = "This contract has been removed from queue."
+    redirect_to "/active_contracts"
+  end
 end
--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,10 +2,10 @@ class GigsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @gig = Gig.create(client_id: params[:client_id],
-                      legal_contract_id: params[:legal_contract_id],
-                      music_contract_id: params[:music_contract_id],
-                      design_contract_id: params[:design_contract_id])
+    @active_contracts = current_user.currently_carted
+    @gig = Gig.create(user_id: current_user.id)
+    @active_contracts.update_all(gig_id: @gig.id, status: "activated")
+    @gig.calculate_total_hours
 
     flash[:success] = 'New Gig Created!'
     redirect_to "/gigs/#{@gig.id}"
@@ -15,4 +15,5 @@ class GigsController < ApplicationController
     @gig = Gig.find(params[:id])
     redirect_to '/' if @gig.user_id != current_user.id
   end
+
 end
